a1c8d18d6743e4d7d06439ae304b5e9496d5cf8f
Victor Fernandes Neto <vvfernandesneto@gmail.com>
2024-05-26 13:05:06 -0400

    feat: implementado o botão de toggle em 'assistido' dinâmico

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';  
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 interface Movie {
   id: number;
@@ -9,6 +8,11 @@ interface Movie {
   diretor: string;
   descricao: string;
   lancamento: string;
+  watched?: boolean;
+}
+
+interface WatchedMovie {
+  filme_id: number;
 }
 
 @Component({
@@ -20,12 +24,44 @@ interface Movie {
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[] = [];
+  watchedMovies: Movie[] = [];
+  user_id: string | null = sessionStorage.getItem('user_id');
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.fetchMovies();
+    this.fetchWatchedStatus();
+  }
+
+  fetchMovies() {
     this.http.get<Movie[]>('http://localhost:3000/movies')
-      .subscribe(data => this.movies = data);
-      console.log(this.movies);
+      .subscribe(data => {
+        this.movies = data;
+      });
+  }
+
+  fetchWatchedStatus() {
+    this.http.get<WatchedMovie[]>('http://localhost:3000/movies/watched?user_uuid=' + this.user_id!)
+      .subscribe(data =>
+        {
+        const watchedMovieIds = new Set(data.map((movie: WatchedMovie) => movie.filme_id));
+
+        this.movies.forEach((movie: Movie) => {
+          movie.watched = watchedMovieIds.has(movie.id);
+        });
+        this.movies.forEach(m => console.log(m.titulo, m.watched));
+      });
+  }
+
+  toggleWatched(movie: Movie) {
+    const watched = !movie.watched;
+    movie.watched = watched;
+
+    this.http.post('http://localhost:3000/movies/watched', {
+      user_uuid: this.user_id,
+      movie_id: movie.id
+    })
+      .subscribe(() => console.log('Movie watched status updated'));
   }
 }
